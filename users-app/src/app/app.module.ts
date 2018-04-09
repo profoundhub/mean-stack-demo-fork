99a90153cae2942b00c895254ec0bd5fade7490f
Daniel <github@profoundideation.com>
2018-04-08 21:57:01 -0400

    update changes since

--- users-app/src/app/app.module.ts
+++ users-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { ClassComponent } from './user/class/class.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ClassComponent
   ],
   imports: [
     BrowserModule
