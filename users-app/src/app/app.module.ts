8a59d9e6d07e885cb31ba3da46a56fae7f90aa76
Daniel <github@profoundideation.com>
2018-04-09 01:38:43 -0400

    + user edits

--- users-app/src/app/app.module.ts
+++ users-app/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { UserDetailsComponent } from './user/user-details/user-details.component
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserNewComponent } from './user/user-new/user-new.component';
 import { UserService } from './user/user.service';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { UserService } from './user/user.service';
     ClassComponent,
     UserDetailsComponent,
     UserListComponent,
-    UserNewComponent
+    UserNewComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
