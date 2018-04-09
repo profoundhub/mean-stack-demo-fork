c56ea4de972634c6247a0c0f9cee87c7adb716ab
Daniel <github@profoundideation.com>
2018-04-09 00:56:45 -0400

    newUser

--- users-app/src/app/user/user-new/user-new.component.ts
+++ users-app/src/app/user/user-new/user-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './../user';
 
 @Component({
   selector: 'app-user-new',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-new.component.css']
 })
 export class UserNewComponent implements OnInit {
+  newUser = new User();
 
   constructor() { }
 
