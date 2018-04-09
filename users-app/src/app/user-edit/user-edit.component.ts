6b132b829abb1d81537e7b298d83600458359c51
Daniel <github@profoundideation.com>
2018-04-09 01:43:10 -0400

    edit

--- users-app/src/app/user-edit/user-edit.component.ts
+++ users-app/src/app/user-edit/user-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-edit',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  @Input() user;
 
   constructor() { }
 
