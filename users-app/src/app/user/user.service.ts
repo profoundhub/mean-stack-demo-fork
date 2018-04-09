f3a75c35d96561dc7cd51af3e928103c8b3de9a2
Daniel <github@profoundideation.com>
2018-04-08 22:48:40 -0400

    correct - fix

--- users-app/src/app/user/user.service.ts
+++ users-app/src/app/user/user.service.ts
@@ -1,8 +1,37 @@
+import { Http } from '@angular/http';
+import { User } from './user';
 import { Injectable } from '@angular/core';
+// import 'rxjs';
+import { Observable } from 'rxjs/Observable';
+
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  constructor(private _http: Http) { }
+
+  create(user: User) {
+    return this._http.post('/users', user).toPromise();
+  }
+
+  destroy(user: User) {
+    return this._http.delete('/users' + user.id).toPromise();
+  }
+
+  update(user: User) {
+    return this._http.put('/users' + user._id, user).toPromise();
+  }
+
+  // getUsers(): Observable<User> {
+  //   return
+  // }
+
+  getUser() {
+    return this._http.get('/users').toPromise();
+  }
+
+  getUsers(user: User) {
+    return this._http.get('/users' + user._id).toPromise();
+  }
 
 }
