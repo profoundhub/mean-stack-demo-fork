15b2ea9dd8d3b2e54e48f7f2ed2a32483ce106f4
Daniel <github@profoundideation.com>
2018-04-08 22:11:03 -0400

    + UserService

--- users-app/src/app/app.module.ts
+++ users-app/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ClassComponent } from './user/class/class.component';
 import { UserDetailsComponent } from './user/user-details/user-details.component';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserNewComponent } from './user/user-new/user-new.component';
+import { UserService } from './user/user.service';
 
 
 @NgModule({
@@ -22,7 +22,7 @@ import { UserNewComponent } from './user/user-new/user-new.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
